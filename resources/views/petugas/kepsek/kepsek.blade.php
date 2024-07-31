fcb83e016ec255c4dd81040220b4d0378fba7b77
Jepi Oktamipa <jefyokta50@gmail.com>
2024-07-31 12:11:57 +0700

    add missing a in kepala

--- resources/views/petugas/kepsek/kepsek.blade.php
+++ resources/views/petugas/kepsek/kepsek.blade.php
@@ -45,7 +45,7 @@
                         <div class="modal-dialog modal-dialog-centered" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title">Form Tambah Kepal Perpustakaan</h5>
+                                    <h5 class="modal-title">Form Tambah Kepala Perpustakaan</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
